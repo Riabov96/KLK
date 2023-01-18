7369398eb0fa3d2329a26ddb91f0a833a9796bb0
Riabov96 <hardbred96@gmail.com>
2023-01-18 19:49:00 +0100

    etsttdtr

--- prueba.js
+++ prueba.js
@@ -1,1 +1,27 @@
-ytgfuy
+var numero = 11;
+    
+    if(numero%2==0){
+        
+        console.log("El número "+numero+" es par.");
+    
+    }else{
+        
+        console.log("El número "+numero+" es impar.");
+    }
+
+    
+
+
+
+    var numero1 = 11;
+    var numero2 = 2;
+    var numero3 = 13;
+    
+    if(numero3==numero1 + numero2){
+        
+        console.log("El número "+numero3+" es la suma de "+numero1+ " y "+numero2+".");
+    
+    }else{
+        
+        console.log("El número "+numero3+"NO es la suma de los anteriores");
+    }
